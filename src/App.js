db4be6ae3bc9775ef108acbecfb373caf1035655
dunabia <161461530+dunabia@users.noreply.github.com>
2025-07-21 14:54:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import UgcGallery from "./components/UgcGallery";
 function App() {
   //Instance ID
   let InstanceId_Widget = "1170762";
-  let InstanceId_QA = "";
+  let InstanceId_QA = "1170763";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "1170764";
